040bdde7ed6f3516687d65efe34fb2c1dda8f9ec
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-05 19:10:26 -0800

    add safeguards to prevent errors from accessing properties of undefined variables

--- functions/get.js
+++ functions/get.js
@@ -4,7 +4,12 @@ const createFile = require('./createFile')
 const getUsersToSuspendAndSave = async () => {
 	try {
 		const usersToSuspend = await getUsersToSuspend()
-		await createFile(usersToSuspend)
+		if (usersToSuspend) {
+			console.log(`potentially ${usersToSuspend.length} users to suspend`)
+			await createFile(usersToSuspend)
+		} else {
+			console.log('Error on execution, usersToSuspend is', usersToSuspend)
+		}
 	} catch (error) {
 		console.log(error)
 	}
