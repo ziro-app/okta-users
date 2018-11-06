b14d96d071584403fc4a099bc58fb48b91079deb
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2018-11-05 16:22:55 -0800

    refactor function to get inactive users to its own file

---
+++ functions/getInactiveUsers.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const get = require('axios').get
+
+const getInactiveUsers = async () => {
+	try {
+		let users = []
+		let after = ''
+		while (true) {
+			/* get the 'after' parameter from link and the users list from data */
+			const { headers: { link }, data } = await get(
+				`${process.env.OKTA_API_URL}?after=${after}`,
+				{ headers: { 'Authorization': `${process.env.OKTA_API_KEY}` }
+			})
+			/* reduce the number of fields returned by each user object */
+			users.push(...data.map( user => {
+				const { id, lastLogin, profile } = user
+				const { firstName, lastName, login } = profile
+				return { id, firstName, lastName, login, lastLogin }
+			}))
+			console.log(`${users.length} users fetched`)
+			/* get the 'after' parameter to allow pagination and access the whole database */
+			if (link)
+				after = link.split('?after=').pop().split('&limit=').shift()
+			/* if there is no more pages left, break the loop */
+			if (!link.includes('rel="next"'))
+				break
+		}
+		console.log('end')
+		/* return the users that didn't login over the past 5 months */
+		return users.filter( ({ lastLogin }) => lastLogin < '2018-06-01' )
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+module.exports = getInactiveUsers
--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-require('dotenv').config()
-const get = require('axios').get
+const getInactiveUsers = require('./functions/getInactiveUsers')
 
-const getUsers = async () => {
+const getInactiveUsersAndSuspend = async () => {
 	try {
-		let users = []
-		let after = ''
-		while (true) {
-			/* get the link (containing the 'after' parameter) and get the users list (data) */
-			const { headers: { link }, data } = await get(
-				`${process.env.OKTA_API_URL}?after=${after}`,
-				{ headers: { 'Authorization': `${process.env.OKTA_API_KEY}` }
-			})
-			/* reduce the number of fields returned by each user object */
-			users.push(...data.map( user => {
-				const { id, lastLogin, profile } = user
-				const { firstName, lastName, login } = profile
-				return { id, firstName, lastName, login, lastLogin }
-			}))
-			console.log(`${users.length} users fetched`)
-			/* get the after parameter to allow pagination and list database fully */
-			if (link)
-				after = link.split('?after=').pop().split('&limit=').shift()
-			/* if there is no more pagination left, break the loop */
-			if (!link.includes('rel="next"'))
-				break
-		}
-		console.log(users)
-		console.log('end')
+		const inactive = await getInactiveUsers()
+		console.log(inactive)
 	} catch (error) {
 		console.log(error)
 	}
 }
 
-getUsers()
+getInactiveUsersAndSuspend()
